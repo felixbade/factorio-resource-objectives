423479b6db22a696256167a71be6ed9e4de9abec
Felix Bade <flix.bade@gmail.com>
2022-12-30 16:45:24 +0200

    Check how many iron plates the player has

--- control.lua
+++ control.lua
@@ -4,7 +4,10 @@ end)
 
 function check_inventory(event)
     local player = game.players[event.player_index]
-    game.print("Checking inventory of "..player.name)
+
+    local item_name = "iron-plate"
+    local item_count = player.get_item_count(item_name)
+    game.print(player.name .. " has " .. item_count .. " of " .. item_name)
 end
 
 script.on_event(defines.events.on_player_main_inventory_changed, check_inventory)
