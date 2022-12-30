fc791658c949e4d47396bde67b0c944ae6332ff5
Felix Bade <flix.bade@gmail.com>
2022-12-30 16:25:01 +0200

    Check player inventory

--- control.lua
+++ control.lua
@@ -1,3 +1,10 @@
 script.on_init(function()
     game.print('initialized')
 end)
+
+function check_inventory(event)
+    game.print("Checking inventory on tick"..event.tick)
+end
+
+script.on_event(defines.events.on_player_main_inventory_changed, check_inventory)
+
