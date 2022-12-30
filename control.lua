47d3ea9b8af6de199f2c70549174201abcbbc40a
Felix Bade <flix.bade@gmail.com>
2022-12-30 21:03:03 +0200

    Add progression to objectives

--- control.lua
+++ control.lua
@@ -1,11 +1,51 @@
-local item_name = "iron-plate"
-local needs_amount = 100
+local objectives = {
+    {
+        item = "stone",
+        quantity = 1,
+        note = "Tip: find some big rocks."
+    },
+    {
+        item = "coal",
+        quantity = 1
+    },
+    {
+        item = "iron-plate",
+        quantity = 100
+    }
+}
+
+function next_objective()
+    global.current_objective = (global.current_objective or 0) + 1
+    if global.current_objective > #objectives then
+        player.set_goal_description("") -- Hide objective pop-up
+    else
+        for _, player in ipairs(game.players) do
+            update_goal_text(player, false)
+        end
+    end
+end
+
+function reward(player)
+    local objective = objectives[global.current_objective]
+    game.print({
+        "",
+        player.name .. " earned " .. objective.quantity .. " x ",
+        {"item-name." .. objective.item}
+    })
+    player.insert{name=objective.item, count=objective.quantity}
+end
 
 script.on_init(function()
+    global.current_objective = 1
     game.print("Initialized")
 end)
 
 function update_goal_text(player, silent)
+    if global.current_objective > #objectives then
+        return
+    end
+    local objective = objectives[global.current_objective]
+
     local inventory = player.get_main_inventory()
 
     local has_amount = 0
@@ -14,18 +54,23 @@ function update_goal_text(player, silent)
         -- crash cut scene, so the has_amount will be 0 in the beginning of
         -- the game even if there is something. The amount will be updated
         -- once the player's inventory is changed in some way.
-        has_amount = inventory.get_item_count(item_name)
+        has_amount = inventory.get_item_count(objective.item)
     end
 
-    local goal = {
+    local goal_description = {
         "", -- A special key for concatenating
         "Have resources in your inventory:\n",
-        "[item=" .. item_name .. "] ",
-        {"item-name." .. item_name},
-        ": " .. has_amount .. "/" .. needs_amount
+        "[item=" .. objective.item .. "] ",
+        {"item-name." .. objective.item},
+        ": " .. has_amount .. "/" .. objective.quantity
     }
 
-    player.set_goal_description(goal, silent)
+    player.set_goal_description(goal_description, silent)
+
+    if has_amount >= objective.quantity then
+        reward(player)
+        next_objective()
+    end
 end
 
 function check_inventory(event)
